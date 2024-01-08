245d2222c64cd3a05d5e0fa9ca7dad89ae1c10c7
j4ggr <reto.jaeggli@gmail.com>
2024-01-08 07:51:55 +0100

    _strings - added limits, excess and anderson darling

--- daspi/_strings.py
+++ daspi/_strings.py
@@ -1,26 +1,60 @@
+import warnings
+
 from os import environ
 from typing import Literal
 from datetime import date
 
-LANG = environ.get('DASPI_LANGUAGE', 'en')
 
 class _String_:
+
+    anderson_darling: dict = {
+        'en': 'Anderson-Darling',
+        'de': 'Anderson-Darling',}
+    
+    lsl: dict = { 
+        'en': 'LSL',
+        'de': 'USG',}
+    
+    usl: dict = {
+        'en': 'USL',
+        'de': 'OSG',}
+    
+    lcl: dict = { 
+        'en': 'LCL',
+        'de': 'UEG',}
+    
+    ucl: dict = {
+        'en': 'UCL',
+        'de': 'OEG',}
+    
+    excess: dict = {
+        'en': 'excess',
+        'de': 'Exzess',}
+    
+    skew: dict = {
+        'en': 'skew',
+        'de': 'Schiefe'}
+
+    LANGUAGE: str = environ.get('DASPI_LANGUAGE', 'en')
     USERNAME: str = environ['USERNAME']
+
     @property
     def TODAY(self):
         return date.today().strftime('%Y.%m.%d')
     
-    anderson_darling: dict = {
-        'en': 'Anderson-Darling'}
-    
     def __getitem__(self, item:str) -> str | Literal['']:
+        empty = ''
         try:
-            txt = getattr(self, item)
-            return txt[LANG]
+            strings = getattr(self, item)
+            return strings[self.LANGUAGE]
         except KeyError:
-            return txt['en']
+            if isinstance(strings, dict):
+                return strings['en']
+            else:
+                raise ArithmeticError
         except AttributeError:
-            return ''
+            warnings.warn(f'No string found for {item}!')
+            return empty
 STR = _String_()
 
 __all__ = [
