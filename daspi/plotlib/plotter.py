Fixed SimpleChart _categorical_feature_axis_. Called it for all axes but should only for one added calculation for lower and upper confidence @ delta_variance_ci function added delta_stdev_ci function

daspi/plotlib/plotter.py
@@ -277,7 +277,7 @@ class Probability(LinearRegression):
         """If given distribution is exponential or logaritmic, change axis
         scale for samples or quantiles (not for percentiles) from 'linear' 
         to 'log'."""
-        xscale, yscale = 'linear', 'linear'
+        xscale = yscale = 'linear'
         if self.dist.name in ('expon', 'log', 'lognorm'):
             if self.kind[1] == 'q': 
                 xscale = 'log'
@@ -511,8 +511,9 @@ class Jitter(_TransformPlotter):
 
 class GaussianKDE(_TransformPlotter):
 
-    __slots__ = ('_height', 'show_density_axis', 'base_on_zero')
-    _height: float
+    __slots__ = ('_height', '_stretch', 'show_density_axis', 'base_on_zero')
+    _height: float | None
+    _stretch: float
     show_density_axis: bool
     base_on_zero: bool
 
@@ -520,6 +521,7 @@ class GaussianKDE(_TransformPlotter):
             self,
             source: Hashable,
             target: str,
+            stretch: float = 1,
             height: float | None = None,
             target_on_y: bool = True,
             color: str | None = None,
@@ -528,6 +530,7 @@ class GaussianKDE(_TransformPlotter):
             base_on_zero: bool = True,
             **kwds) -> None:
         self._height = height
+        self._stretch = stretch
         self.show_density_axis = show_density_axis
         self.base_on_zero = base_on_zero
         feature = PLOTTER.TRANSFORMED_FEATURE
@@ -542,11 +545,16 @@ class GaussianKDE(_TransformPlotter):
     def height(self) -> float:
         """Height of kde curve at its maximum."""
         return self._height
+    @property
+    def stretch(self) -> float:
+        """Factor by which the curve was stretched in height"""
+        return self._stretch
         
     def transform(
             self, feature_data: float | int, target_data: Series) -> DataFrame:
         sequence, estimation = estimate_kernel_density(
-            target_data, height=self.height, base=feature_data)
+            data=target_data, stretch=self.stretch, height=self.height,
+            base=feature_data)
         data = pd.DataFrame({
             self.target: sequence,
             self.feature: estimation,
@@ -585,10 +593,13 @@ class Violine(GaussianKDE):
             color: str | None = None,
             ax: Axes | None = None,
             **kwds) -> None:
-        self._height = width/2
+        if feature:
+            stretch = width*PLOTTER.VIOLINE_STRETCH/(2*CATEGORY.FEATURE_SPACE)
+        else:
+            stretch = 1
         super().__init__(
-            source=source, target=target, feature=feature, height=self.height,
-            target_on_y=target_on_y, color=color, ax=ax,
+            source=source, target=target, feature=feature, stretch=stretch,
+            height=None, target_on_y=target_on_y, color=color, ax=ax,
             show_density_axis=True, base_on_zero=False, **kwds)
 
     def __call__(self, **kwds) -> None:
@@ -605,9 +616,6 @@ class Violine(GaussianKDE):
 
 class Ridge(GaussianKDE):
 
-    __slots__ = ('stretch')
-    stretch: float
-
     def __init__(
             self,
             source: Hashable,
@@ -617,8 +625,6 @@ class Ridge(GaussianKDE):
             color: str | None = None,
             ax: Axes | None = None,
             **kwds) -> None:
-        _, estim = estimate_kernel_density(source[target])
-        self.stretch = 1/np.max(estim)
         super().__init__(
             source=source, target=target, feature=feature,
             target_on_y=target_on_y, color=color, ax=ax,
@@ -626,13 +632,12 @@ class Ridge(GaussianKDE):
 
     def transform(
             self, feature_data: float | int, target_data: Series) -> DataFrame:
-        f_base = feature_data + PLOTTER.RIDGE_SHIFT
         sequence, estimation = estimate_kernel_density(
-            target_data, stretch=self.stretch, base=f_base)
+            target_data, stretch=PLOTTER.VIOLINE_STRETCH, base=feature_data)
         data = pd.DataFrame({
             self.target: sequence,
             self.feature: estimation,
-            PLOTTER.F_BASE_NAME: f_base * np.ones(len(sequence))})
+            PLOTTER.F_BASE_NAME: feature_data * np.ones(len(sequence))})
         return data
     
     def __call__(self, **kwds) -> None:

daspi/statistics/confidence.py
@@ -168,11 +168,7 @@ def stdev_ci(
     ci_low, ci_upp : float
         lower and upper confidence level
     """
-    s2, ci_low2, ci_upp2 = variance_ci(sample, level, n_groups)
-    s = sqrt(s2)
-    ci_low = sqrt(ci_low2)
-    ci_upp = sqrt(ci_upp2)
-    return s, ci_low, ci_upp
+    return tuple(map(sqrt, variance_ci(sample, level, n_groups)))
 
 def proportion_ci(
         count: int, nobs: int, level: float = 0.95, n_groups: int = 1
@@ -206,7 +202,7 @@ def proportion_ci(
     return portion, ci_low, ci_upp
 
 def bonferroni_ci(
-        data: DataFrame, sample: str, feature: str, level: float = 0.95, 
+        data: DataFrame, target: str, feature: str, level: float = 0.95, 
         ci_func: Callable = stdev_ci, n_groups: int | None = None, 
         name: str='midpoint') -> pd.DataFrame:
     """Calculate confidence interval after bonferroni correction.
@@ -223,8 +219,8 @@ def bonferroni_ci(
     ----------
     data : pandas.DataFrame
         data frame containing sample and feature data
-    sample : str
-        name of sample data column
+    target : str
+        name of target sample data column
     feature : str
         name of categorical feature. The confidence intervals are 
         calculated separately for these groups
@@ -249,7 +245,7 @@ def bonferroni_ci(
     """
     columns = [name, 'ci_low', 'ci_upp']
     if not isinstance(feature, list): feature = [feature]
-    groups = data.groupby(feature)[sample]
+    groups = data.groupby(feature)[target]
     n_groups = n_groups if isinstance(n_groups, int) else groups.ngroups
     df = pd.DataFrame(
             groups.agg(lambda x: ci_func(x, level, n_groups)).dropna().to_list(),
@@ -281,12 +277,13 @@ def delta_mean_ci(x1: Iterable, x2: Iterable, level: float = 0.95
     ci_low, ci_upp : float
         lower and upper confidence level
     """
-    alpha = confidence_to_alpha(level, two_sided=True)                          # two sided significance level
-    n1, n2 = len(x1), len(x2)                                                   # sample sizes
-    s1, s2 = np.var(x1, ddof=1), np.var(x2, ddof=1)                             # sample variances
-    s = np.sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))                # pooled standard deviation
-    dof = n1 + n2 - 2                                                            # degrees of freedom
-    t_crit = t.ppf(1 - alpha, dof)                                               # t-critical value for 95% CI
+    alpha = confidence_to_alpha(level, two_sided=True)
+    n1, n2 = len(x1), len(x2)
+    s1, s2 = np.var(x1, ddof=1), np.var(x2, ddof=1)
+    # pooled standard deviation:
+    s = np.sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
+    dof = n1 + n2 - 2
+    t_crit = t.ppf(1 - alpha, dof)
     m1, m2 = np.mean(x1), np.mean(x2)
     delta = m1 - m2
     ci_low = (m1 - m2) - t_crit * np.sqrt(1 / len(x1) + 1 / len(x2)) * s
@@ -294,7 +291,6 @@ def delta_mean_ci(x1: Iterable, x2: Iterable, level: float = 0.95
 
     return delta, ci_low, ci_upp
 
-#TODO: delta_variance_ci lower and upper not implemented yet
 def delta_variance_ci(x1: Iterable, x2: Iterable, level: float = 0.95
         ) -> Tuple[float, float, float]:
     """two sided confidence interval for variance difference of two
@@ -311,13 +307,49 @@ def delta_variance_ci(x1: Iterable, x2: Iterable, level: float = 0.95
     -------
     delta : float
         difference of variance of data
-    ci_low, ci_upp : np.nan
-        NOT IMPLEMENTED YET
+    ci_low, ci_upp : float
+        lower and upper confidence level
+    
+    Notes
+    -----
+    This function is a ChatGPT solution and therefore does not guarantee
+    that this solution is correct.
     """
-    s1, s2 = np.var(x1, ddof=1), np.var(x2, ddof=1)
-    delta = s1 - s2
-    return delta, np.nan, np.nan
+    alpha = confidence_to_alpha(level, two_sided=True)  
+    s2_1, s2_2 = np.var(x1, ddof=1), np.var(x2, ddof=1)
+    dof1, dof2 = len(x1) - 1, len(x2) - 1
+    delta = s2_1 - s2_2
+    F_upp = f.ppf(alpha, dof1, dof2)
+    F_low = f.ppf(1 - alpha, dof1, dof2)
+    ci_low = (s2_1 / s2_2) * F_low
+    ci_upp = (s2_1 / s2_2) * F_upp
+    return delta, ci_low, ci_upp
 
+def delta_stdev_ci(x1: Iterable, x2: Iterable, level: float = 0.95
+        ) -> Tuple[float, float, float]:
+    """two sided confidence interval for standard deviation difference 
+    of two independent variables.
+
+    Parameters
+    ----------
+    x1, x2 : array like
+        sample data, Only one-dimensional sample are accepted
+    ci : float in (0, 1), optional
+        confidence level, by default 0.95
+
+    Returns
+    -------
+    delta : float
+        difference of standard deviation of data
+    ci_low, ci_upp : float
+        lower and upper confidence level
+    
+    Notes
+    -----
+    This function is a ChatGPT solution and therefore does not guarantee
+    that this solution is correct.
+    """
+    return tuple(map(sqrt, delta_variance_ci(x1, x2, level)))
 
 def delta_proportions_ci(
         count1: int, nobs1: int, count2: int, nobs2: int,